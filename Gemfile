a7e6a07beb4aef37c3363a177216d92ebb95117b
Morton Fox <github@qslw.com>
2021-04-29 12:53:39 -0400

    Render QR codes in SVG

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source 'https://rubygems.org'
 
 gem 'prawn'
+gem 'prawn-svg'
 gem 'rqrcode'
 
 __END__
--- createpdf.rb
+++ createpdf.rb
@@ -3,33 +3,33 @@
 # frozen_string_literal: true
 
 require 'prawn'
+require 'prawn-svg'
 require 'rqrcode'
 
-def generate infile, outfile
+def generate(infile, outfile)
   codes = File.open(infile) { |f| f.each_line.map(&:chomp) }
 
-  pngs = codes.map { |code|
-    qr = RQRCode::QRCode.new(code, level: :m)
-    qr.as_png(
-      bit_depth: 1,
-      color_mode: ChunkyPNG::COLOR_GRAYSCALE,
-      color: 'black',
-      file: nil,
-      fill: 'white'
-    ).to_s
+  qrimages = codes.first(80).map { |code|
+    RQRCode::QRCode.new(code, level: :m).as_svg
   }
 
   Prawn::Document.generate(outfile, margin: [21, 6]) do
     # Maximum 80 QR codes on the page.
     # 8 QR codes per row. 10 rows.
-    pngs.first(80).each_slice(8).each_with_index { |pngrow, row|
-      pngrow.each_with_index { |png, col|
-        # Each QR code is 75x75 PDF points, leaving 12 points for horizontal
-        # margins and 42 points for vertical margins on a 8.5x11 inch page.
-        image(
-          StringIO.new(png),
-          at: [col * 75, bounds.height - row * 75],
-          fit: [75, 75]
+    qrimages.each_slice(8).each_with_index { |imgrow, row|
+      imgrow.each_with_index { |img, col|
+        # In a PDF, one inch on the page is 72 points. Coordinates start from
+        # (0,0) at the bottom left of the page. The first coordinate increases
+        # as we go from left to right and the second increases as we go from
+        # bottom to top.
+        # Each QR code is within a box of size 75x75 points, leaving 12 points
+        # for horizontal margins and 42 points for vertical margins on a
+        # 8.5x11 inch page. Within the box, we shrink the QR code so there is
+        # a 5-point border around the QR code.
+        svg(
+          img,
+          at: [col * 75 + 5, bounds.height - row * 75 - 5],
+          width: 65, height: 65
         )
       }
     }
